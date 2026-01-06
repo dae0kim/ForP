a572cfc4ca0079cbc2da468753de6897dc3ed244
dae0kim <rlaeo28@gmail.com>
2026-01-06 13:23:46 +0900

    미사용 기능 제거

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 import { router } from "./app/router";
-
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,8 +17,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
-
